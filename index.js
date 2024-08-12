d8c371aad5ff97b1aa98ec991eae14778087dadd
22CB006 <aryalakshmi.m2022csbs@sece.ac.in>
2024-08-12 12:08:57 +0530

    feat: cors

--- index.js
+++ index.js
@@ -5,29 +5,31 @@ const userRoutes = require("./routes/userRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const auctionRoutes = require("./routes/auctionRoutes");
+const cors = require("cors");
 const contactRoutes = require("./routes/contactRoutes");
 const mongoose = require("mongoose");
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/ecommerce")
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((error) => {
-        console.error("MongoDB connection error:", error);
-    });
+mongoose
+  .connect("mongodb://localhost:27017/ecommerce")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
 
 app.use(express.json());
+app.use(cors());
 
 // Use routes
 app.use("/product", productRoutes);
 app.use("/user", userRoutes);
 app.use("/cart", cartRoutes);
 app.use("/order", orderRoutes);
-app.use("/auction", auctionRoutes); 
+app.use("/auction", auctionRoutes);
 app.use("/contact", contactRoutes);
 
-
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
